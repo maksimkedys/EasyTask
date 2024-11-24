1cb2d7b5921496eaf5e969bcdcbaf94266027083
maksimkedys <maksimkedys@icloud.com>
2024-11-24 02:11:41 +0200

    update user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,8 +7,8 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath(): string {
     return `assets/users/${this.avatar}`;
